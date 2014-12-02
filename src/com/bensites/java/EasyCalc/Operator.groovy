49558d0b7fdef6f60f408a111f0bfd1a76e06d37
Ben! <penne12@allforfunmc.com>
2014-12-02 14:39:54 -0600

    Adds to Registry

--- src/com/bensites/java/EasyCalc/Operator.groovy
+++ src/com/bensites/java/EasyCalc/Operator.groovy
@@ -1,12 +1,12 @@
 package com.bensites.java.EasyCalc
 
 class Operator {
-	 Closure<Double> operation = {}
+	 Closure<Double> operation = {return 0.0}
 	 String symbols = []
 	 Operator(String[] Symbols, Closure<Double> Operation){
 		 operation = Operation
 		 symbols = Symbols
-
+		 addTo(Main.Registry)
 	 }
 	 HashMap<String, Closure> addTo(HashMap<String, Closure> Registry) {
 		 symbols.each {
