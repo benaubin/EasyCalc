d75ec9ef49b424c6c2089204f9eceaa5a33a183d
Ben! <penne12@allforfunmc.com>
2014-12-05 18:20:25 -0600

    If it's not a number. Don't show it.

--- src/com/bensites/java/EasyCalc/MainGui.java
+++ src/com/bensites/java/EasyCalc/MainGui.java
@@ -2,13 +2,13 @@ package com.bensites.java.EasyCalc;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 
 public class MainGUI extends JDialog {
 	private JPanel contentPane;
 	private JButton buttonCalculate;
 	private JTextField equationField;
-	private JLabel Answer;
 
 	public MainGUI(){
 		setContentPane(contentPane);
@@ -17,36 +17,26 @@ public class MainGUI extends JDialog {
 
 		buttonCalculate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				equationField.setText(equationField.getText() + " = " + Main.parser.run(Main.parser.stringToArray(equationField.getText())).toString());
+				if (Main.parser.run(Main.parser.stringToArray(equationField.getText())).toString() != "NaN")
+					equationField.setText(equationField.getText() + " = " +
+							Main.parser.run(Main.parser.stringToArray(equationField.getText())).toString());
 			}
 		});
 
-
-
-// call onCancel() when cross is clicked
+// call dispose() when cross is clicked
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e){
-				onCancel();
+				dispose();
 			}
 		});
 
-// call onCancel() on ESCAPE
+// call dispose() on ESCAPE
 		contentPane.registerKeyboardAction(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				onCancel();
+				dispose();
 			}
 		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-	}
-
-	private void onOK(){
-// add your code here
-		dispose();
-	}
-
-	private void onCancel(){
-// add your code here if necessary
-		dispose();
 	}
 
 	public static void main(String[] args){
