9775f36d5b5d5670d7c6784975bad8b376872fbb
Ben! <penne12@allforfunmc.com>
2014-12-11 11:02:32 -0600

    Dynamic Updates

--- src/com/bensites/java/EasyCalc/GUI/MainGUI.java
+++ src/com/bensites/java/EasyCalc/GUI/MainGUI.java
@@ -6,28 +6,35 @@ import javax.swing.*;
 import java.awt.event.*;
 
 
-public class MainGUI extends JDialog {
+public class MainGUI extends JDialog implements KeyListener {
 	private JPanel contentPane;
 	private JTextField equationField;
 	private JLabel Answer;
+	public void keyTyped(KeyEvent e) {
+		String equation = equationField.getText() + e.getKeyChar();
+		if (Main.parser.run(Main.parser.stringToArray(equation)).toString() != "NaN")
+			Answer.setText("= " +
+					Main.parser.run(Main.parser.stringToArray(equation)).toString());
+		else {
+			Answer.setText("");
+		}
+		if (equation.equals("")){
+			Answer.setText("");
+		}
+	}
+	public void keyPressed(KeyEvent e) {
+
+	}
+	public void keyReleased(KeyEvent e) {
+
+	}
+
 
 	public MainGUI(){
 		setContentPane(contentPane);
 		setModal(true);
+		equationField.addKeyListener(this);
 
-		equationField.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				if (Main.parser.run(Main.parser.stringToArray(equationField.getText())).toString() != "NaN")
-					Answer.setText("= " +
-							Main.parser.run(Main.parser.stringToArray(equationField.getText())).toString());
-				else Answer.setText("Error!");
-				if (equationField.getText().equals("")){
-					Answer.setText("");
-				}
-			}
-		});
-
-// call dispose() when cross is clicked
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e){
