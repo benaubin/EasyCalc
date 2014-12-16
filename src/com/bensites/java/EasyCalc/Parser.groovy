1a4029f240b67853f2b7452cef23b3f4d2c4ed04
Ben! <penne12@allforfunmc.com>
2014-12-16 11:08:28 -0600

    spacing

--- src/com/bensites/java/EasyCalc/Parser.groovy
+++ src/com/bensites/java/EasyCalc/Parser.groovy
@@ -4,16 +4,18 @@ import com.bensites.java.EasyCalc.Util.Console.Console
 class Parser {
 
 	final Console console
+
 	public Parser(Console c) {
 		console = c
 	}
-	ArrayList<String> stringToArray(String s){
+
+	ArrayList<String> stringToArray(String s) {
 		StringBuilder temp = new StringBuilder()
 		def array = []
 		s.toCharArray().each {
-			if(it != ' ')
+			if (it != ' ')
 				temp.append it
-			else{
+			else {
 				array.add temp.toString()
 				temp = new StringBuilder()
 			}
@@ -21,47 +23,53 @@ class Parser {
 		array.add temp.toString()
 		return array
 	}
-	def containsGrouping(ArrayList<String> equation){
+
+	def containsGrouping(ArrayList<String> equation) {
 		boolean containsGrouping = false
-		for(it in equation) {
-			if(it.toString().startsWith("(") || it.toString().endsWith(")")) containsGrouping = true
+		for (it in equation) {
+			if (it.toString().startsWith("(") || it.toString().endsWith(")")) containsGrouping = true
 		}
 		containsGrouping
 	}
-	def removeFromString(int index, String string){
+
+	def removeFromString(int index, String string) {
 		def t = new StringBuffer()
 		t.append(string)
-		if(index != -1)
+		if (index != -1)
 			t.deleteCharAt(index)
 		else
 			t.deleteCharAt(t.length() - 1)
 		t.toString()
 	}
+
 	def run(ArrayList<String> equation) {
 		if (containsGrouping(equation)) {
 			equation.each {
+				if(it.startsWith("(")){
 
+				} else if (it.endsWith(")")){
+					
+				}
 			}
 		} else {
-			try{
+			try {
 				Main.order.each { level ->
 					for (i in 0..equation.size() - 1)
-						if ((level.contains(equation[i]) ) ||
+						if ((level.contains(equation[i])) ||
 								(level.contains("Other") && Main.getRegistry().containsKey(equation[i]))) {
-							def answer = Main.runOperator(equation[i - 1],equation[i],equation[i + 1])
+							def answer = Main.runOperator(equation[i - 1], equation[i], equation[i + 1])
 							for (a in 1..3) equation.remove(i - 1); equation.add(i - 1, answer);
 						}
-					}
-			if (equation.size() > 1)
-				return run(equation)
-			else
-				return equation[0]
+				}
+				if (equation.size() > 1)
+					return run(equation)
+				else
+					return equation[0]
 			} catch (Exception e) {
 				return Double.NaN
-			} catch (StackOverflowError e){
+			} catch (StackOverflowError e) {
 				return Double.NaN
 			}
 		}
 	}
-	private beforeParse = [Closure]
 }
