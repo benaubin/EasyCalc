b053cdcd94731b31d6bde8f6f0e1c9f9e63b52af
Ben! <penne12@allforfunmc.com>
2014-12-16 11:40:09 -0600

    Some Stuff

--- src/com/bensites/java/EasyCalc/Parser.groovy
+++ src/com/bensites/java/EasyCalc/Parser.groovy
@@ -43,33 +43,23 @@ class Parser {
 	}
 
 	def run(ArrayList<String> equation) {
-		if (containsGrouping(equation)) {
-			equation.each {
-				if(it.startsWith("(")){
-
-				} else if (it.endsWith(")")){
-					
-				}
+		try {
+			Main.order.each { level ->
+				for (i in 0..equation.size() - 1)
+					if ((level.contains(equation[i])) ||
+							(level.contains("Other") && Main.getRegistry().containsKey(equation[i]))) {
+						def answer = Main.runOperator(equation[i - 1], equation[i], equation[i + 1])
+						for (a in 1..3) equation.remove(i - 1); equation.add(i - 1, answer);
+					}
 			}
-		} else {
-			try {
-				Main.order.each { level ->
-					for (i in 0..equation.size() - 1)
-						if ((level.contains(equation[i])) ||
-								(level.contains("Other") && Main.getRegistry().containsKey(equation[i]))) {
-							def answer = Main.runOperator(equation[i - 1], equation[i], equation[i + 1])
-							for (a in 1..3) equation.remove(i - 1); equation.add(i - 1, answer);
-						}
-				}
-				if (equation.size() > 1)
-					return run(equation)
-				else
-					return equation[0]
-			} catch (Exception e) {
-				return Double.NaN
-			} catch (StackOverflowError e) {
-				return Double.NaN
-			}
+			if (equation.size() > 1)
+				return run(equation)
+			else
+				return equation[0]
+		} catch (Exception e) {
+			return Double.NaN
+		} catch (StackOverflowError e) {
+			return Double.NaN
 		}
 	}
 }
