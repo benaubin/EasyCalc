ac136d7e695a3d441766da977754d588469039ef
Ben! <ben@bensites.com>
2015-01-14 18:58:49 -0600

    The ECalMod is complete!

--- src/com/bensites/java/EasyCalc/Other/ECalMod.groovy
+++ src/com/bensites/java/EasyCalc/Other/ECalMod.groovy
@@ -8,11 +8,16 @@ import com.bensites.java.EasyCalc.Main
 class ECalMod extends File{
 
     final String name
+    final String url
 
     final String author
     final String authorLink
 
-    def config
+    public def config = [:]
+
+    final String modHelp;
+
+    final HashMap<String, HashMap<String, String>> operatorHelp = [:]
 
     ECalMod(File ModDir){
         super(ModDir.toURI())
@@ -20,10 +25,29 @@ class ECalMod extends File{
         name = config.mod.name
         author = config.mod.author.name
         authorLink = config.mod.author.homepage
+        url = config.mod.url
+        modHelp = config.help.mod
+
         Main.console.println("Loading $name by $author")
         (String[]) config.operators.each {
             Main.console.println("\tLoading: " + it)
             Main.registerOperator(this,new File(this, "${it}.ecal"))
+            operatorHelp.put(it, config.help.operators.get(it))
         }
+        Main.mods.add(this)
+
+    }
+    @Override
+    String toString(){
+        name
+    }
+    String getHelp(String operator){
+        operatorHelp[operator]["Help"]
+    }
+    String getUsage(String operator){
+        operatorHelp[operator]["Usage"]
+    }
+    String[] getOperators() {
+        config.operators
     }
 }
