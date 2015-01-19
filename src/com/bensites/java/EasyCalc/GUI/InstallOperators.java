8d4f51eb6cdb7f1cda0b1eca21419c8ca9d6ec5e
Ben! <ben@bensites.com>
2015-01-19 11:04:09 -0600

    Ask user to relaunch.

--- src/com/bensites/java/EasyCalc/GUI/InstallOperators.java
+++ src/com/bensites/java/EasyCalc/GUI/InstallOperators.java
@@ -1,14 +1,17 @@
 package com.bensites.java.EasyCalc.GUI;
 
 import com.bensites.java.EasyCalc.Main;
+import org.apache.tools.ant.taskdefs.Exec;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class InstallOperators extends JDialog {
 	private JPanel contentPane;
 	private JButton buttonNO;
 	private JButton buttonYES;
+	private JLabel question;
 
 	public InstallOperators(){
 		setContentPane(contentPane);
@@ -17,7 +20,14 @@ public class InstallOperators extends JDialog {
 		buttonYES.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
 				Main.useSuggestedOperators();
-				dispose();
+				buttonNO.setVisible(false);
+				buttonYES.setVisible(true);
+				question.setText("Please relaunch EasyCalc");
+				try {
+					Thread.sleep(5000);
+				} catch(Exception ex){
+				}
+				System.exit(0);
 			}
 		});
 		buttonNO.addActionListener(new ActionListener() {
@@ -27,4 +37,5 @@ public class InstallOperators extends JDialog {
 			}
 		});
 	}
+
 }
