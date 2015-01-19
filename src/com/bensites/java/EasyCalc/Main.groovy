e90e2f111bf2549615edbc937a5d94b68815e258
Ben! <ben@bensites.com>
2015-01-19 11:04:36 -0600

    Spacing

--- src/com/bensites/java/EasyCalc/Main.groovy
+++ src/com/bensites/java/EasyCalc/Main.groovy
@@ -22,26 +22,27 @@ class Main {
 	 * Minor: Asks user to trigger an operators installer
 	 * Fix: Does nothing
 	 */
-	static final version = ["1","2","0"]
+	static final version = ["1", "2", "0"]
 	static File easyCalcFolder = new File("EasyCalc/")
 	static File operatorsFile
 	static File orderFile
 	static File modsFolder
 	static File versionFile
 	static ArrayList<ArrayList<String>> order = []
-	static LinkedHashMap<String,Closure> Registry = [:]
-	static LinkedHashMap<String,LinkedHashMap> meta = [:]
+	static LinkedHashMap<String, Closure> Registry = [:]
+	static LinkedHashMap<String, LinkedHashMap> meta = [:]
 	static ArrayList<ECalMod> mods = []
 
 	final static Console console = new Console()
 	static shell = new GroovyShell()
 	public static Parser parser
+
 	static main(args) {
 		console.println "Your OS:" + System.getProperty("os.name")
-		if(System.getProperty("os.name") == "Mac OS X")
+		if (System.getProperty("os.name") == "Mac OS X")
 			easyCalcFolder = new File("${System.getProperty("user.home")}/Library/Application Support/EasyCalc")
 		else console.println "EasyCalc isn't fully supported with your OS. Please paste this into a GitHub issue."
-		if(!easyCalcFolder.exists()) easyCalcFolder.mkdirs()
+		if (!easyCalcFolder.exists()) easyCalcFolder.mkdirs()
 		printTitle(console)
 		console.println "Welcome to EasyCal"
 		console.println "Written by Ben of bensites.com"
@@ -49,32 +50,32 @@ class Main {
 			MainGUI mainGUI = new MainGUI()
 			mainGUI.pack()
 			mainGUI.setVisible(true)
-		},"GUIthread")
-		def loadingSteps = ["Filing files","Operating Operators","Finishing up","Loading complete"]
+		}, "GUIthread")
+		def loadingSteps = ["Filing files", "Operating Operators", "Finishing up", "Loading complete"]
 		def loadingBar = new ProgressBar(console, loadingSteps)
-		console.print(loadingBar,true,true)
+		console.print(loadingBar, true, true)
 		parser = new Parser(console)
 		//Create files
 		operatorsFile = new File(easyCalcFolder, "operators.ecal")
 		orderFile = new File(easyCalcFolder, "order.ecal")
 		modsFolder = new File(easyCalcFolder, "mods")
 		versionFile = new File(easyCalcFolder, "version.ecal")
-		if(!operatorsFile.exists()){
+		if (!operatorsFile.exists()) {
 			operatorsFile.createNewFile()
 			operatorsFile.setText(Files.DefaultOperators)
 		}
-		if(!orderFile.exists()){
+		if (!orderFile.exists()) {
 			orderFile.createNewFile()
 			orderFile.setText(Files.Order)
 		}
-		if(!modsFolder.exists()){
+		if (!modsFolder.exists()) {
 			modsFolder.mkdirs()
 		}
 		loadingBar.progress()
 		//Load all operators
 		load()
 		loadingBar.progress()
-		if(updatedMinor()){
+		if (updatedMinor()) {
 			versionFile.createNewFile()
 			versionFile.text = version.join(".")
 			InstallOperators installOperators = new InstallOperators()
@@ -86,13 +87,15 @@ class Main {
 		GUIThread.start()
 	}
 
-	ConsoleMessage println(Object toPrint){
+	ConsoleMessage println(Object toPrint) {
 		console.println(toPrint)
 	}
-	ConsoleMessage print(Object toPrint){
+
+	ConsoleMessage print(Object toPrint) {
 		console.print(toPrint)
 	}
-	static void printTitle(Console console){
+
+	static void printTitle(Console console) {
 		console.println "  ______                 _____      _      "
 		console.println " |  ____|               / ____|    | |     "
 		console.println " | |__   __ _ ___ _   _| |     __ _| | ___ "
@@ -103,14 +106,15 @@ class Main {
 		console.println "                  |___/   "
 	}
 
-	static void useSuggestedOperators(){
+	static void useSuggestedOperators() {
 		addMod(Files.Suggested, "EasyCalc Addons")
 	}
-	static String runOperator(String argLeft, String operator, String argRight){
+
+	static String runOperator(String argLeft, String operator, String argRight) {
 		def answer
 		try {
 			answer = getRegistry()[operator](Double.parseDouble(argLeft), Double.parseDouble(argRight), meta.get(operator))
-		}catch (NumberFormatException e){
+		} catch (NumberFormatException e) {
 			answer = getRegistry()[operator](argLeft, argRight, meta.get(operator))
 		}
 		if (answer instanceof LinkedHashMap) {
@@ -119,51 +123,64 @@ class Main {
 		} else
 			return (String) answer
 	}
-	static void reload(){
+
+	static void reload() {
 		console.println("Reloading")
 		load()
 	}
-	private static void load(){
-		Registry = shell.evaluate("["+operatorsFile.getText()+"]")
+
+	private static void load() {
+		Registry = shell.evaluate("[" + operatorsFile.getText() + "]")
 		order = (shell.evaluate("[\n${orderFile.getText()}\n]"))
 		modsFolder.eachFileRecurse {
-			if(it.isDirectory()){
+			if (it.isDirectory()) {
 				new ECalMod(it)
 			}
 		}
 	}
-	public static void registerOperator(ECalMod mod, File operator){
-		shell.evaluate("{ x, y, meta ->"+operator.getText()+"}")
-		meta.put(removeEcal(operator.name),[
-				"file":operator, "mod":mod])
+
+	public static void registerOperator(ECalMod mod, File operator) {
+		shell.evaluate("{ x, y, meta ->" + operator.getText() + "}")
+		meta.put(removeEcal(operator.name), [
+				"file": operator, "mod": mod])
 	}
-	static String removeEcal(String fileName){
+
+	static String removeEcal(String fileName) {
 		fileName.take(fileName.length() - 5)
 	}
-	static File addMod(LinkedHashMap operators, String name){
+
+	static File addMod(LinkedHashMap operators, String name) {
 		File modFolder = new File(modsFolder, name)
 		modFolder.mkdirs()
 		operators.each {
-			def operator = new File(modFolder, (String) it.key + ".ecal")
+			File operator;
+			if (it.key != "modinfo")
+				operator = new File(modFolder, (String) it.key + ".ecal")
+			else
+				operator = new File(modFolder, (String) it.key)
 			operator.createNewFile()
-			operator.setText((String)it.value)
+			operator.setText((String) it.value)
 		}
 		modFolder
 	}
 
-	static String getOrderFileText(){
+	static String getOrderFileText() {
 		orderFile.getText()
 	}
-	static void setOrderFileText(String s){
+
+	static void setOrderFileText(String s) {
 		orderFile.text = s
 	}
-	static boolean updatedMinor(){
-		!versionFile.exists() || !versionFile.text.startsWith([version[0],version[1]].join("."))
+
+	static boolean updatedMinor() {
+		!versionFile.exists() || !versionFile.text.startsWith([version[0], version[1]].join("."))
 	}
-	static boolean updatedMajor(){
-		null == versionFile || !versionFile.exists() || !versionFile.text.startsWith([version[0],version[1]].join("."))
+
+	static boolean updatedMajor() {
+		null == versionFile || !versionFile.exists() || !versionFile.text.startsWith([version[0], version[1]].join("."))
 	}
-	static String version(){
+
+	static String version() {
 		version.join(".")
 	}
 }
